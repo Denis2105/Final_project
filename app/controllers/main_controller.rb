227526706cbe009747581c2ff99fc40e2814ae38
Denis Ng <denisng89@gmail.com>
2016-09-16 00:21:41 +1000

    near final commit

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,34 +7,49 @@ class MainController < ApplicationController
 
     page = agent.get('https://www.ozbargain.com.au/')
     body = Nokogiri::HTML(page.body)
-    # binding.pry
 
     Bargain.destroy_all
 
-    body.css('.node').each do |bargain|
 
-      title = bargain.css('h2 a').text
-      linkNode = bargain.css('.title a').attr('href')
-      link = 'https://www.ozbargain.com.au' + linkNode
-      description = bargain.css('.content p').text
+    page_number = 1
 
-      bargains = []
+    loop do
 
-      if bargain.css('.right img')[0]
-        image_url = bargain.css('.right img')[0]['src']
-      else
-        image_url = nil
+      body.css('.node-ozbdeal').each do |bargain|
+
+        title = bargain.css('h2 a').text
+        linkNode = bargain.css('.title a').attr('href')
+        link = 'https://www.ozbargain.com.au' + linkNode
+        description = bargain.css('.content p').text
+
+        bargains = []
+
+        if bargain.css('.right img')[0]
+          image_url = bargain.css('.right img')[0]['src']
+        else
+          image_url = nil
+        end
+
+        # saving new bargains into db scrapped from the site
+        @bargain = Bargain.new
+        @bargain.title = title
+        @bargain.description = description
+        @bargain.image_url = image_url
+        @bargain.bargain_url = link
+        @bargain.save
+
       end
 
+      page_number += 1
+      next_link = page.links_with(:text => page_number.to_s)[0]
 
-      @bargain = Bargain.new
-      @bargain.title = title
-      @bargain.description = description
-      @bargain.image_url = image_url
-      @bargain.bargain_url = link
-      @bargain.save
+      # need a way out of the loop
+      break if next_link == nil
+
+      body = next_link.click
 
     end
+    # end
 
     @bargains = Bargain.all
     # puts title
@@ -46,13 +61,44 @@ class MainController < ApplicationController
 
   def results
 
-    @search = params[:search]
+    # search method that redirects user to results site
+    search = params[:search]
+
+    # "%" + search + "%" prevents user from accessing our database via the search function, safety measures from hackers
+    # ilike searches case insensitive keywords
+    @bargain_results = Bargain.where("title ilike ? ", "%#{search}%")
 
   end
 
-
 end
 
-# render json: @bargain.to_json
-# scrape ozbargain using the search key from params
-# save it to your db
+
+
+#
+# while page_num < 9
+#
+#   # scrape current page
+#   # save to db
+#   results_bargains = next_page_link.click
+#   listing_container_menulog = results_menulog.css('#searchResultBlock')
+#   listing_menulog = listing_container_menulog.css('.content')
+#   # page_num += 1
+#   # clicks on next page
+#
+# end
+
+
+
+
+#   next_page_link = results_menulog.link_with(:text => 'NEXT ')
+#
+#   if next_page_link
+#     next_page = true
+#     results_menulog = next_page_link.click
+#     listing_container_menulog = results_menulog.css('#searchResultBlock')
+#     listing_menulog = listing_container_menulog.css('.content')
+#   else
+#     next_page = false
+#   end
+#
+# end until next_page == false
